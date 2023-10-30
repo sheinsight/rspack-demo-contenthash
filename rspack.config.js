fe6c2bbdd8f50b61c42bc46403d86d0bcee7c6ee
ityuany <519495771@qq.com>
2023-10-30 14:30:43 +0800

    html-webpack-plugin has error

--- rspack.config.js
+++ rspack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+// console.log(HtmlWebpackPlugin);
 
 function getSrcLessLoaderRspack() {
   return {
@@ -70,71 +73,34 @@ module.exports = {
   },
   cache: false,
   target: ["web", "es5"],
-
-  optimization: {
-    minimize: true,
-    // realContentHash: true,
-    splitChunks: {
-      chunks: "all",
-      cacheGroups: {
-        vendor: {
-          name: "chunk-vendors",
-          test: /[\\/]node_modules[\\/]/,
-          chunks: "initial",
-          priority: 7,
-          reuseExistingChunk: true,
-        },
-        sheinComponents: {
-          name: "chunk-shein-components",
-          test: /[\\/]@shein-components[\\/]/,
-          chunks: "initial",
-          priority: 8,
-          reuseExistingChunk: true,
-        },
-        shineout: {
-          name: "chunk-shineout",
-          test: /[\\/](shineout)[\\/]/,
-          chunks: "initial",
-          priority: 9,
-          reuseExistingChunk: true,
-        },
-        react: {
-          name: "chunk-react",
-          test: /[\\/](react|react-dom)[\\/]/,
-          chunks: "initial",
-          priority: 10,
-          reuseExistingChunk: true,
-        },
-      },
-    },
-    runtimeChunk: true,
-    moduleIds: "deterministic",
-  },
+ 
   module: {
     rules: [
       {
         test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|ico|otf)$/i,
         type: "asset",
       },
-
+      
       getNodeModulesLessLoaderRspack(),
       getNodeModulesCssLoaderRspack(),
       getSrcCssLoaderRspack(),
       getSrcLessLoaderRspack(),
     ],
   },
-  builtins: {
-    presetEnv: {
-      targets: ["chrome 100"],
-    },
-
-    progress: {
-      prefix: "progressPrefix",
-    },
-    react: {
-      runtime: "automatic",
-    },
-  },
+  plugins:[
+    new HtmlWebpackPlugin()
+  ],
+  // builtins: { 
+  //   progress: {
+  //     prefix: "progressPrefix",
+  //   }, 
+  // },
   mode: "production",
   devtool: "hidden-source-map",
+  experiments:{
+    rspackFuture: {
+      disableTransformByDefault: true,
+      newResolver: true,
+    },
+  }
 };
